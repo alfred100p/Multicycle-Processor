79aa60946a23dac08df22be74fac6b1d7eb8df48
Alfred W. Jacob <82844187+alfred100p@users.noreply.github.com>
2021-05-03 19:04:48 +0530

    add dff and reg+ update proc

--- processor.v
+++ processor.v
@@ -1,8 +1,10 @@
-module procesor();
+`include "adder_16.v"
+module procesor(clk);
+input clk;
 reg[15:0] pc;
-int pc_count;
+int pc_count,pc_nu;//variable used to store content of program counter useful for cases like jump
 bit instr_f;
-
+adder_16 ad(pc_count,16'd16,instr_f,1'd0,pc_nu,_);
 initial
 begin
 pc_count=0;
@@ -13,7 +15,8 @@ always@(instr_f)
 begin
 
 //asign mem[15:0]pc=[pc_count+15:pc_count]instr_mem
-pc_count+=16;
+#10//wait for add 
+pc_count=pc_nu;
 //if instr_d happens always asfter instr_f set it to 1
 instr_f=0;
 end
