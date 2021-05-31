33e3e80e601d3d1b6691591321b6980443d9a674
Alfred William Jacob <60570372+aLefred0@users.noreply.github.com>
2021-06-01 00:04:04 +0530

    Move files to individual files

--- h_adder.v
+++
@@ -1,9 +0,0 @@
-module h_adder(a,b,clk,s,c);
-input  a,b,clk;
-output reg s,c;
-always@(posedge clk)
-begin
-s=a^b;
-c=a&b;
-end
-endmodule
--- reg_loader.v
+++
@@ -1,9 +0,0 @@
-module reg_loader(ip,on,op);
-input [15:0] ip;
-input on;
-output reg [15:0] op;
-always @(posedge on)
-begin
-op=ip;
-end
-endmodule
